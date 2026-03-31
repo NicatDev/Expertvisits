1c6d93a3ffa98c9e5ff866e79a8973cee0bc3bec
NicatDev <nicat254memmedov@gmail.com>
2026-03-31 22:30:56 +0400

    mai

--- frontend_platform/src/components/layout/Footer/index.jsx
+++ frontend_platform/src/components/layout/Footer/index.jsx
@@ -7,7 +7,7 @@ import styles from './style.module.scss';
 import LegalModal from './LegalModal';
 
 const Footer = () => {
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
     const [modalConfig, setModalConfig] = useState({ isOpen: false, type: 'privacy' });
 
     const openModal = (e, type) => {
